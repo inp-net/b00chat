f4f9d2bc9140a4f8c76f6627eaa98d58ba517572
Matteo Planchet <matteo.planchet@gmail.com>
2025-12-12 17:44:40 +0100

    remove pronouns display (sorry gwenn)

--- src/lib/components/Sender.svelte
+++ src/lib/components/Sender.svelte
@@ -9,23 +9,16 @@
 <script lang="ts">
 	const maxSenderNameLength = 25;
 
-	let { senderName, senderColor, senderPronouns }: SenderProps = $props();
+	let { senderName, senderColor }: SenderProps = $props();
 </script>
 
 <span class="sender-name" style="color: {senderColor}">
 	{senderName.slice(0, maxSenderNameLength)}
 </span>
-{#if senderPronouns}
-	<span class="sender-pronouns"> ({senderPronouns})</span>
-{/if}
 
 <style>
 	.sender-name {
 		font-weight: bold;
 		color: var(--color-high-contrast);
-	}
-
-	.sender-pronouns {
-		color: var(--color-low-contrast);
 	}
 </style>
