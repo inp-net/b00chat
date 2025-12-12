3d0b64be61bc6955d06fa18ff32e345084514e34
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-12-12 11:10:22 +0100

    refactor: remove unused env vars

--- src/lib/server/env.ts
+++ src/lib/server/env.ts
@@ -1,4 +1,4 @@
-import arkenv from 'arkenv';
+import arkenv, { type } from 'arkenv';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,10 +9,6 @@ export const env = arkenv({
 	CLIENT_SECRET: 'string.alphanumeric',
 	REDIRECT_URL: 'string.url',
 	LOGOUT_URL: 'string.url',
-	AUTHENTIK_URL: 'string.url',
-	AUTHORIZE_URL: 'string.url',
-	TOKEN_URL: 'string.url',
-	USER_INFO_URL: 'string.url',
 	BANNED_UIDS: [/^[\w\d,]+$/, '=>', (uids) => uids.split(',')],
 	ADMIN_UIDS: [/^[\w\d,]+$/, '=>', (uids) => uids.split(',')],
 	// TODO
